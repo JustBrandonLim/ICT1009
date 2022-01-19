1639a58dd6bcfc2559fc05e5f0b5293fd0dd83d5
Brandon <brandonlimjingzhi@gmail.com>
2022-01-19 16:52:56 +0800

    Added Lab2

---
+++ Java/Lab1/src/io/github/justbrandonlim/lab1/task2/Task2.java
@@ -0,0 +1,24 @@
+package io.github.justbrandonlim.lab1.task2;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while (true)
+		{
+			System.out.println("Enter the amount in USD");
+			float usd = scanner.nextFloat();
+			
+			if (usd < 0)
+				break;
+			else
+				System.out.println("The final amount in SGD " + usd * 1.35 );
+		}
+		
+		scanner.close();
+	}
+
+}
---
+++ Java/Lab1/src/io/github/justbrandonlim/lab1/task3/Task3.java
@@ -0,0 +1,17 @@
+package io.github.justbrandonlim.lab1.task3;
+
+public class Task3 {
+
+	public static void main(String[] args) {
+		long totalMilliseconds = System.currentTimeMillis();
+		long totalSeconds = totalMilliseconds / 1000;
+		int currentSecond = (int) (totalSeconds % 60);
+		long totalMinutes = totalSeconds / 60;
+		int currentMinute = (int) (totalMinutes % 60);
+		long totalHours = totalMinutes / 60;
+		int currentHour = (int) (totalHours % 24);
+		
+		System.out.printf("Current time is %d:%d:%d GMT", currentHour, currentMinute, currentSecond);
+	}
+
+}
